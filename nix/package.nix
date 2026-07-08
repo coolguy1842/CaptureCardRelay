de19cf54c8df229bcc571bb0d3293acdd38fff73
coolguy1842 <coolguy1842@proton.me>
2026-07-08 17:04:47 +1000

    bump version

--- nix/package.nix
+++ nix/package.nix
@@ -1,6 +1,6 @@
 { pkgs, lib, commandLineArgs ? "", ... }: pkgs.stdenv.mkDerivation {
     name = "CaptureCardRelay";
-    version = "1.9.1";
+    version = "1.9.2";
     src = ../.;
 
     nativeBuildInputs = with pkgs; [
